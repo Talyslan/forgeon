912e157d0cb5bc472870296aaedb2e4d864b204d
Talyslan <talyslancpc@gmail.com>
2026-03-03 01:36:43 -0300

    chore: add prettier specifications

--- eslint.config.js
+++ eslint.config.js
@@ -6,21 +6,21 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
-  },
-  tseslint.configs.recommended,
-  eslintConfigPrettier,
-  {
-    plugins: {
-      "simple-import-sort": simpleImportSort,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.node },
     },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
+    tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        plugins: {
+            "simple-import-sort": simpleImportSort,
+        },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
+        },
     },
-  },
 ]);
